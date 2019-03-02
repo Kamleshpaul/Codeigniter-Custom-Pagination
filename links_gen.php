f1c2fc2d4876a3d392b70c0aec35901a9f40fa08
Kamlesh Paul <kamleshpaul414@gmail.com>
2019-03-02 17:30:36 +0530

    Update links_gen.php

--- links_gen.php
+++ links_gen.php
@@ -6,3 +6,26 @@
 					?>
 			<a href="javascript:void(0)" class="<?php echo ($start ==$page)? 'active':''?>"  onClick="nextPage('<?php echo $page ?>')"><?php echo $i; ?></a >
 <?php	}	?>
+
+					
+					
+// ajax call to change start value						
+<script>
+function nextPage(limt){	
+			
+var url1= window.location.href;
+
+
+		$.ajax({
+                     url: url1,
+                     type: "POST",
+                     data: {limt},
+                     success: function (response) {
+                    
+		$('#NextPage').html(response);
+               
+                     },
+                });
+				}
+				
+</script>								
